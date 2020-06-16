e684c6bff11e6c16465c5caf4554368995c1c4cb
Izek Greenfield <igreenfield@axiomsl.com>
2020-06-16 17:27:08 +0300

    [SPARK-26208][SQL] add headers to empty csv files when header=true

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/UnivocityGenerator.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/UnivocityGenerator.scala
@@ -33,7 +33,6 @@ private[csv] class UnivocityGenerator(
   private val writerSettings = options.asWriterSettings
   writerSettings.setHeaders(schema.fieldNames: _*)
   private val gen = new CsvWriter(writer, writerSettings)
-  private var printHeader = options.headerFlag
 
   // A `ValueConverter` is responsible for converting a value of an `InternalRow` to `String`.
   // When the value is null, this converter should not be called.
@@ -74,15 +73,19 @@ private[csv] class UnivocityGenerator(
     values
   }
 
+  def writeHeaders(): Unit = {
+    gen.writeHeaders()
+  }
+
   /**
    * Writes a single InternalRow to CSV using Univocity.
    */
   def write(row: InternalRow): Unit = {
-    if (printHeader) {
-      gen.writeHeaders()
-    }
     gen.writeRow(convertRow(row): _*)
-    printHeader = false
+  }
+
+  def writeToString(row: InternalRow): String = {
+    gen.writeRowToString(convertRow(row): _*)
   }
 
   def close(): Unit = gen.close()
